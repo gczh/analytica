9f8058e78badf3c5d95a9a1a4d54e3fa94855dc1
Gabriel Chuan <czh.gabriel@gmail.com>
2014-02-19 21:14:35 +0800

    basic code with slim and mongoid installed. sample included to show it is working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Analytica::Application.config.secret_key_base = '2c5396923ff1722da4195d72af5c286be05dfad8889f4d0c6d6a0d4cc86bcc1a2a07423d701b9c7df8f9f4c43888ffba791deb37e862c4133a09e55081cea444'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Analytica::Application.config.session_store :cookie_store, key: '_analytica_session'
